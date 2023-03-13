19a3e368399c283736fdd6d7e764c4f1638907f7
ArjunAce <arjun.mudhaliyar@gmail.com>
2023-03-14 00:09:04 +0530

    Added scroll bar in wordsWrapper

--- src/components/WordsContainer/index.js
+++ src/components/WordsContainer/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import styles from "./styles.module.scss"
-import Button from "components/Button";
-import Separator from "components/Separator";
+import scroll from "assets/images/scroll.png";
+import scrollBottom from "assets/images/scroll-bottom.png";
 
 function WordsContainer() {
     return (
         <div className={styles['words-container']}>
             Words
-
+            <div className={styles['scroll-wrapper']}>
+                <img src={scroll} alt="scroll" />
+                <img className={styles['scroll-bottom']} src={scrollBottom} alt="scroll" />
+            </div>
         </div>
     )
 }
