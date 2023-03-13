aa860ea0919addfebe096342af11ef0700a7c87b
ArjunAce <arjun.mudhaliyar@gmail.com>
2023-03-14 00:24:29 +0530

    Added words styles

--- src/components/WordsContainer/index.js
+++ src/components/WordsContainer/index.js
@@ -3,10 +3,13 @@ import styles from "./styles.module.scss"
 import scroll from "assets/images/scroll.png";
 import scrollBottom from "assets/images/scroll-bottom.png";
 
+const words = ['soft', 'monster', 'drummer', 'flying', 'space', 'paint', 'road', 'jam', 'website'];
+
 function WordsContainer() {
     return (
         <div className={styles['words-container']}>
-            Words
+            <div className={styles['title']}>Words to search</div>
+            {words.map((x, i) => (<div className={styles['word']} key={i}>{x}</div>))}
             <div className={styles['scroll-wrapper']}>
                 <img src={scroll} alt="scroll" />
                 <img className={styles['scroll-bottom']} src={scrollBottom} alt="scroll" />
