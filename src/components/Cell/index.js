9f8b9cb6e4169cdd45d108dbc4f8a75b0a6ad470
ArjunAce <arjun.mudhaliyar@gmail.com>
2023-03-16 22:52:54 +0530

    Added editable cell functionality

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -1,14 +1,39 @@
-import React from 'react'
 import styles from "./styles.module.scss"
 import cell from "assets/images/cell.png";
+import React, { useState } from 'react';
 
-function Cell({ letter }) {
+const Cell = ({ letter }) => {
+    const [editable, setEditable] = useState(false);
+    const [currentLetter, setCurrentLetter] = useState(letter);
+    const [tempValue, setTempValue] = useState('');
+
+    const handleLetterClick = () => {
+        setEditable(true);
+        setTempValue(currentLetter);
+        setCurrentLetter('');
+    };
+
+    const handleChange = (event) => {
+        setCurrentLetter(event.target.value.toUpperCase());
+        setEditable(false);
+    };
+
+    const handleBlur = () => {
+        if (currentLetter === '') {
+            setCurrentLetter(tempValue);
+        }
+    };
     return (
         <div className={styles['cell']}>
             <img src={cell} alt='cell background' />
-            <span>{letter}</span>
+            {editable || currentLetter === '' ? (
+                <input type="text" value={currentLetter} onChange={handleChange} onBlur={handleBlur}
+                    maxLength="1" autoFocus />
+            ) : (
+                <span onClick={handleLetterClick}>{currentLetter}</span>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default Cell;
