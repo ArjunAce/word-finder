ff2dec822518383304f3a14f5f752bf63d7fd62f
ArjunAce <arjun.mudhaliyar@gmail.com>
2023-03-16 23:19:48 +0530

    added cross on every cell

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -14,23 +14,29 @@ const Cell = ({ letter, updateLetter, row, col }) => {
     };
 
     const handleChange = (event) => {
+        // console.log("handleChange");
         updateLetter(event.target.value.toUpperCase(), row, col);
         setEditable(false);
     };
 
     const handleBlur = () => {
+        // console.log("handleBlur");
         if (currentLetter === '') {
             setCurrentLetter(tempValue);
         }
+        setEditable(false);
     };
     return (
         <div className={styles['cell']}>
             <img src={cell} alt='cell background' />
             {editable || currentLetter === '' ? (
+                <>
                 <input type="text" value={currentLetter} onChange={handleChange} onBlur={handleBlur}
                     maxLength="1" autoFocus />
+                    <span className={styles['cross']} title="Delete cell"></span>
+                </>
             ) : (
-                <span onClick={handleLetterClick}>{currentLetter}</span>
+                <span className={styles['letter']} onClick={handleLetterClick}>{currentLetter}</span>
             )}
         </div>
     );
